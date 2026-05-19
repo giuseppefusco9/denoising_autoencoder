cf0292f47ef3cc26a2ce11bfc0b3301cb6351298
Giuseppe Fusco <giuseppefusco@Air-di-Giuseppe.fritz.box>
2026-05-19 21:11:48 +0200

    add genSummary file

---
+++ genSummaryTab.py
@@ -0,0 +1,26 @@
+import torch
+from torchinfo import summary
+from conv_ae_model import ConvAutoencoderDenoise
+
+# 1. Inizializza il tuo modello
+model = ConvAutoencoderDenoise(in_channels=3, out_channels=3)
+
+# 2. Definisci la grandezza di un'immagine in input finta (Batch, Canali, Altezza, Larghezza)
+# Usa la dimensione effettiva a cui hai ritagliato il tuo dataset, ad esempio 256x256
+dimensione_input = (1, 3, 256, 256)
+
+print("\n" + "="*60)
+print(" SUMMARY DELL'AUTOENCODER (STILE KERAS)")
+print("="*60)
+
+# 3. Genera e stampa la tabella esatta
+# Passiamo col_names per forzare le colonne identiche a quelle della tua foto
+statistiche = summary(
+    model, 
+    input_size=dimensione_input,
+    col_names=["output_size", "num_params"],
+    col_width=20,
+    row_settings=["var_names"]
+)
+
+print(statistiche)
